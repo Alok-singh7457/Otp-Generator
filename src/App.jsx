58407692d791b6f69c9bb02866135c51bf4e516b
alok-singh7457 <askabaap@gmail.com>
2024-09-06 15:41:47 +0530

    adding twailwind and cleaning index

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   
+  const [value , setValue] = useState(5)
 
   return (
-   <>
-        <h1>Setup a Counter</h1>
+   <>  {value}
+        <div>
+        <h1>Setup a Counter</h1> hfdsoshvglokafdsjvkkajfdbv
+        <button onClick={() => setValue(value+1)}>r4rr+</button>
+        {value}
+        <button onClick={ () => setValue(value-1)}>-</button>
+        </div>
+{value}
+
    </>
   )
 }
