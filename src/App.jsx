19f17296bf2179a24a62934cf3bda83c5638600f
alok-singh7457 <askabaap@gmail.com>
2024-09-10 10:19:41 +0530

    adding some css classes and cleaning some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-  const [value , setValue] = useState(5)
+
+
 
   return (
-   <>  {value}
-        <div>
-        <h1>Setup a Counter</h1> hfdsoshvglokafdsjvkkajfdbv
-        <button onClick={() => setValue(value+1)}>r4rr+</button>
-        {value}
-        <button onClick={ () => setValue(value-1)}>-</button>
-        </div>
-{value}
+    <>
+      <div className='bg-gray-600'>
 
-   </>
+        <div className="header w-64 m-auto text-2xl font-bold pt-3 h-16 ">Otp-Generator</div>
+
+        <div className="box w-[90vw] m-auto bg-slate-400 h-[90vh] flex-col px-3 pt-4 rounded-xl">
+        <input type="text"
+        className='h-11 w-96 rounded-xl outline-none text-xl'
+        placeholder='Otp - Generator '
+        />
+        <p>Chracter  <input type="checkbox" name="" id="" /></p>
+        <p>Range:5 <input type="range" name="" id="" /></p>
+       
+        
+
+        </div>
+      </div>
+
+
+    </>
   )
 }
 
