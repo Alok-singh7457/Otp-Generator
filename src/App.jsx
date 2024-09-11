54e043a4ebefaa14abd0ecdbd90b0d4aa77da1b4
alok-singh7457 <askabaap@gmail.com>
2024-09-11 12:51:55 +0530

    using callback and  useEffect to optimize and make this to production Quality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   
 const [rangeValue , setRangeValue] = useState(4)
 const [allowedChar , setAllowedChar] = useState(false)
- const chracter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const [otp, setOtp] = useState('') 
+ 
+const otpGenerator = useCallback (() => {
+   let otp = ''
+    let str = '1234567890'
+  let chracter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  
+  let combine = (str + chracter)
 
-const otpGenerator = () => {
-  let otp = ''
       if(allowedChar != true){
         for (let i= 1; i <= rangeValue ; i++){
           otp += Math.floor(Math.random()*10)
@@ -17,14 +22,17 @@ const otpGenerator = () => {
     } 
       } else {
           for (let i = 1 ; i <= rangeValue; i++){
-             otp += chracter.charAt(Math.floor(Math.random()*chracter.length + 1))
-             console.log(otp)
-          }
+             otp += combine.charAt(Math.floor(Math.random()*chracter.length + 1))
+              }
       console.log(otp)
       }
   return otp;
-  }
-  const otp = otpGenerator();
+  },[allowedChar , rangeValue])
+ 
+  useEffect(() => {
+    const newOtp = otpGenerator();  // Generate a new OTP
+    setOtp(newOtp);
+  }, [rangeValue, allowedChar, otpGenerator])
   
 
 
@@ -47,17 +55,13 @@ const otpGenerator = () => {
 
           <p>Chracter  <input type="checkbox" name="" id=""
           onChange={(e) => { 
-            if (e.target.checked) {
-              setAllowedChar(true); // Call your function when checkbox is checked
-            }
+           setAllowedChar((prev) => !prev)
           }} 
          /></p>
           <p>Range:{rangeValue} <input type="range" name="" id="" onChange={(e) => setRangeValue(e.target.value)} 
           min={4}
           max={10}
           /></p>
-
-
 
         </div>
       </div>
